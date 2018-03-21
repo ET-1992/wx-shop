462b576d15f4392147b26e980e583420e78b3b2a
ccj <1147843332@qq.com>
2018-03-21 14:47:59 +0800

    接口修改

--- src/utils/api/hei.js
+++ src/utils/api/hei.js
@@ -12,126 +12,131 @@ const host = 'https://api.wpweixin.com/';
  * requestType: 默认request, [request, uploadFile]
  **/
 const apis = {
-	login: {
-		// path: `api2/auth.signon.json?appid=${APPID}`,
-		path: 'api2/auth.signon.json',
-		method: 'POST',
-	},
-	fetchHome: {
-		path: `api/mag.shop.home.json`,
-	},
-	fetchProductList: {
-		path: `api/mag.product.list.json`,
-	},
-	fetchProduct: {
-		path: `api/mag.product.get.json`,
-	},
-	fetchCartList: {
-		path: `api/mag.cart.get.json`,
-		isForceToken: true,
-	},
-	addCart: {
-		path: `api/mag.cart.add.json`,
-		method: 'POST',
-		isForceToken: true
-	},
-	updateCart: {
-		path: `api/mag.cart.update.json`,
-		method: 'POST',
-		isForceToken: true
-	},
-	removeCart: {
-		path: `api/mag.cart.remove.json`,
-		method: 'POST',
-		isForceToken: true
-	},
-	clearCart: {
-		path: `api/mag.cart.clear.json`,
-		method: 'POST',
-		isForceToken: true
-	},
-	fetchOrderList: {
-		path: `api/mag.order.list.json`,
-		isForceToken: true
-	},
-	fetchOrder: {
-		path: `api/mag.order.get.json`,
-		isForceToken: true
-	},
-	fetchOrderCount: {
-		path: `api/mag.order.counts.json`,
-		isForceToken: true
-	},
-	createOrderAndPay: {
-		path: `api/mag.order.create.json?pay`,
-		method: 'POST',
-		isForceToken: true
-	},
-	createOrder: {
-		path: `api/mag.order.create.json`,
-		method: 'POST',
-		isForceToken: true
-	},
-	payOrder: {
-		path: 'api/mag.order.pay.json',
-		method: 'POST',
-		isForceToken: true
-	},
-	closeOrdery: {
-		path: 'api/mag.order.close.json',
-		method: 'POST',
-		isForceToken: true
-	},
-	confirmOrder: {
-		path: 'api/mag.order.confirm.json',
-		method: 'POST',
-		isForceToken: true
-	},
-	refund: {
-		path: 'api/mag.order.refund.json',
-		method: 'POST',
-		isForceToken: true
-	},
-	fetchLogistics: {
-		path: 'api/mag.order.logistics.json',
-		isForceToken: true
-	},
-	fetchCategory: {
-		path: 'api/mag.product.category.list.json',
-	},
-	createGroupon: {
-		path: 'api/mag.groupon.create.json?pay',
-		method: 'POST',
-		isForceToken: true
-	},
-	joinGroupon: {
-		path: 'api/mag.groupon.join.json?pay',
-		method: 'POST',
-		isForceToken: true
-	},
-	fetchGroupon: {
-		path: 'api/mag.groupon.get.json',
-		isForceToken: true,
-	},
-	fetchCouponList: {
-		path: 'api/mag.coupon.list.json',
-	},
-	fetchMyCouponList: {
-		path: 'api/mag.coupon.my.json',
-		method: 'POST',
-		isForceToken: true,
-	},
-	receiveCoupon: {
-		path: 'api/mag.coupon.receive.json',
-		method: 'POST',
-		isForceToken: true,
-	},
-	upload: {
-		path: 'api2/upload.media.json',
-		isForceToken: true,
-		method: 'POST',
-		requestType: 'uploadFile',
-	},
+    login: {
+        // path: `api2/auth.signon.json?appid=${APPID}`,
+        path: 'api2/auth.signon.json',
+        method: 'POST',
+    },
+    fetchHome: {
+        path: `api/mag.shop.home.json`,
+    },
+    fetchProductList: {
+        path: `api/mag.product.list.json`,
+    },
+    fetchProduct: {
+        path: `api/mag.product.get.json`,
+    },
+    fetchCartList: {
+        path: `api/mag.cart.get.json`,
+        isForceToken: true,
+    },
+    addCart: {
+        path: `api/mag.cart.add.json`,
+        method: 'POST',
+        isForceToken: true
+    },
+    updateCart: {
+        path: `api/mag.cart.update.json`,
+        method: 'POST',
+        isForceToken: true
+    },
+    removeCart: {
+        path: `api/mag.cart.remove.json`,
+        method: 'POST',
+        isForceToken: true
+    },
+    clearCart: {
+        path: `api/mag.cart.clear.json`,
+        method: 'POST',
+        isForceToken: true
+    },
+    fetchOrderList: {
+        path: `api/mag.order.list.json`,
+        isForceToken: true
+    },
+    fetchOrder: {
+        path: `api/mag.order.get.json`,
+        isForceToken: true
+    },
+    fetchOrderCount: {
+        path: `api/mag.order.counts.json`,
+        isForceToken: true
+    },
+    createOrderAndPay: {
+        path: `api/mag.order.create.json?pay&v2`,
+        method: 'POST',
+        isForceToken: true
+    },
+    createOrder: {
+        path: `api/mag.order.create.json`,
+        method: 'POST',
+        isForceToken: true
+    },
+    payOrder: {
+        path: 'api/mag.order.pay.json?pay&v2',
+        method: 'POST',
+        isForceToken: true
+    },
+    peanutPayOrder: {
+        path: 'api/mag.order.pay.peanut.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    closeOrdery: {
+        path: 'api/mag.order.close.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    confirmOrder: {
+        path: 'api/mag.order.confirm.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    refund: {
+        path: 'api/mag.order.refund.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    fetchLogistics: {
+        path: 'api/mag.order.logistics.json',
+        isForceToken: true
+    },
+    fetchCategory: {
+        path: 'api/mag.product.category.list.json',
+    },
+    createGroupon: {
+        path: 'api/mag.groupon.create.json?pay&v2',
+        method: 'POST',
+        isForceToken: true
+    },
+    joinGroupon: {
+        path: 'api/mag.groupon.join.json?pay&v2',
+        method: 'POST',
+        isForceToken: true
+    },
+    fetchGroupon: {
+        path: 'api/mag.groupon.get.json',
+        isForceToken: true,
+    },
+    fetchCouponList: {
+        path: 'api/mag.coupon.list.json',
+    },
+    fetchMyCouponList: {
+        path: 'api/mag.coupon.my.json',
+        method: 'POST',
+        isForceToken: true,
+    },
+    receiveCoupon: {
+        path: 'api/mag.coupon.receive.json',
+        method: 'POST',
+        isForceToken: true,
+    },
+    upload: {
+        path: 'api2/upload.media.json',
+        isForceToken: true,
+        method: 'POST',
+        requestType: 'uploadFile',
+    },
 };
 
 export default new API({ apis, host });
