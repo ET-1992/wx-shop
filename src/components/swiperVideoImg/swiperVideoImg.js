b62fd2bd79b2d489eb4b0b59f8a69e3dd61b9147
谍 <wb-pfd301086@alibaba-inc.com>
2018-06-04 12:47:41 +0800

    feat: 增加视频图片切换组件 产品详情页修改

---
+++ src/components/swiperVideoImg/swiperVideoImg.js
@@ -0,0 +1,66 @@
+Component({
+	properties: {
+		video: {
+			type: String,
+			value: ''
+		},
+    poster: {
+      type: String,
+      value: ''
+    },
+    images: {
+      type: Array,
+      value: []
+    }
+	},
+	data: {
+		type: 'poster',
+    videoPlay: false,
+    videoTime: 0,
+    swiperIndex: 0
+  },
+  methods: {
+    changeType(e) {
+      const type = e.currentTarget.dataset.type;
+      if (this.data.type === type) {
+        return ;
+      }
+      if (type === 'video') {
+        this.setData({
+          type,
+          videoTime: this.data.videoTime
+        })
+      } else if (type === 'img') {
+        this.setData({
+          type,
+          swiperIndex: this.data.swiperIndex
+        })
+      }
+    },
+    touchstart(e) {
+      this.data.clineX = e.touches[0].clientX;
+    },
+    touchmove(e) {
+    },
+    touchend(e) {
+      if (e.changedTouches[0].clientX - this.data.clineX < -70) {
+        this.setData({
+          type: 'img'
+        })
+      }
+    },
+    setVideoTime(e) {
+      this.data.videoTime = e.detail.currentTime;
+    },
+    end() {
+      this.setData({
+        type: 'poster',
+        videoTime: 0
+      })
+    },
+    setSwiperIndex(e) {
+      this.data.swiperIndex = e.detail.current;
+    }
+  }
+});
+
