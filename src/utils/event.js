aecc24a7782496418f7898a9033643cd819affce
谍 <wb-pfd301086@alibaba-inc.com>
2018-06-10 13:24:26 +0800

    feat:增加订阅发布模式

--- src/utils/event.js
+++ src/utils/event.js
@@ -1,51 +1,40 @@
 //event.js
 class Event {
-    on (event, fn, ctx) {
-        if (typeof fn != "function") {
+    constructor()  {
+        this.events = {};
+    }
+    on (name, callback, ctx) {
+        if (typeof callback != "function") {
             console.error('fn must be a function')
             return
         }
         
-        this._stores = this._stores || {}
-        
-        ;(this._stores[event] = this._stores[event] || []).push({cb: fn, ctx: ctx})
-    }
-    emit (event) {
-        this._stores = this._stores || {}
-        var store = this._stores[event], args
-        if (store) {
-            store = store.slice(0)
-            args = [].slice.call(arguments, 1)
-            for (var i = 0, len = store.length; i < len; i++) {
-                store[i].cb.apply(store[i].ctx, args)
-            }
+        var tuple = [ctx, callback];
+        var callbacks = this.events[name];
+        if (Array.isArray(callbacks)) {
+            callbacks.push(tuple);
+        }
+        else {
+            this.events[name] = [tuple];
         }
     }
-    off (event, fn) {
-        this._stores = this._stores || {}
-        // all
-        if (!arguments.length) {
-            this._stores = {}
-            return
+    emit (name, data) {
+        var callbacks = this.events[name];
+        if (Array.isArray(callbacks)) {
+            callbacks.map((tuple) => {
+                var self = tuple[0];
+                var callback = tuple[1];
+                callback.call(self, data);
+            })
         }
-        // specific event
-        var store = this._stores[event]
-        if (!store) return
-        // remove all handlers
-        if (arguments.length === 1) {
-            delete this._stores[event]
-            return 
+    }
+    off (name, ctx) {
+        var callbacks = this.events[name];
+        if (Array.isArray(callbacks)) {
+            this.events[name] = callbacks.filter((tuple) => {
+                return tuple[0] != ctx;
+            })
         }
-        // remove specific handler
-        var cb
-        for (var i = 0, len = store.length; i < len; i++) {
-            cb = store[i].cb
-            if (cb === fn) {
-                store.splice(i, 1)
-                break
-            }
-        }
-        return
-    }   
+    }
 };
-  export default EventEmitter;
+  export default Event;
