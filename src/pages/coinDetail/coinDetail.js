d9f85ec0e307403b8ccd297fb04c8412e20c91ca
well505 <316376955@qq.com>
2018-06-11 18:40:36 +0800

    fix:金币明细

--- src/pages/coinDetail/coinDetail.js
+++ src/pages/coinDetail/coinDetail.js
@@ -1,51 +1,51 @@
+import api from 'utils/api';
+import { formatTime } from 'utils/util';
 Page({
     data:{
-        coinList: [
-            {
-                title: "分享红包",
-                time: "2018-05-22 12:00:23",
-                increaseNum: 89
-            },
-            {
-                title: "分享红包",
-                time: "2018-05-22 12:00:23",
-                increaseNum: 89
-            },
-            {
-                title: "订单抵扣",
-                time: "2018-05-22 12:00:23",
-                decreaseNum: 289
-            },
-        ]
+        next_cursor: 0,
+        isRefresh: true,
     },
     onLoad: function (options) {
+        this.getCoinList()
         wx.setNavigationBarTitle({
             title: '金币明细'
         });
     },
-    onReady:function(){
-        // 生命周期函数--监听页面初次渲染完成
+    async  getCoinList() {
+		const { next_cursor, isRefresh} = this.data;
+		const queryOption = { cursor: next_cursor };
+		if (isRefresh) {
+			wx.showLoading();
+		}
+        const data = await api.hei.wallet(queryOption);
+        wx.hideLoading();
         
+        for(let item in data.data) {
+            data.data[item].formatTime = formatTime(new Date(data.data[item].time * 1000));
+        }
+
+        this.setData({
+            coinList: data.data,
+            isRefresh: false,
+            next_cursor: data.next_cursor,
+        });
+        console.log(this.data.coinList);
     },
-    onShow:function(){
-        // 生命周期函数--监听页面显示
-        
+    
+    
+    /**
+    * 页面相关事件处理函数--监听用户下拉动作
+    */
+    async onPullDownRefresh() {
+        this.setData({ isRefresh: true, next_cursor: 0 });
+        await this.getCoinList();
+        wx.stopPullDownRefresh();
     },
-    onHide:function(){
-        // 生命周期函数--监听页面隐藏
-        
-    },
-    onUnload:function(){
-        // 生命周期函数--监听页面卸载
-       
-    },
-    onPullDownRefresh: function() {
-        // 页面相关事件处理函数--监听用户下拉动作
-        
-    },
-    onReachBottom: function() {
-        // 页面上拉触底事件的处理函数
-        
+
+    async onReachBottom() {
+        const { next_cursor } = this.data;
+        if (!next_cursor) { return; }
+        this.getCoinList();
     },
     onShareAppMessage: function() {
         
