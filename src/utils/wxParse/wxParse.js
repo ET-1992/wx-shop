96376a6a4a6a7c2ebcc97023b190dea9fc282b17
well505 <316376955@qq.com>
2018-06-12 16:41:22 +0800

    fix:富文本图片设置最大宽度,去掉图片预览

--- src/utils/wxParse/wxParse.js
+++ src/utils/wxParse/wxParse.js
@@ -61,12 +61,12 @@ function wxParseImgTap(e) {
   var that = this;
   var nowImgUrl = e.target.dataset.src;
   var tagFrom = e.target.dataset.from;
-  if (typeof (tagFrom) != 'undefined' && tagFrom.length > 0) {
-    wx.previewImage({
-      current: nowImgUrl, // 当前显示图片的http链接
-      urls: that.data[tagFrom].imageUrls // 需要预览的图片http链接列表
-    })
-  }
+  // if (typeof (tagFrom) != 'undefined' && tagFrom.length > 0) {
+  //   wx.previewImage({
+  //     current: nowImgUrl, // 当前显示图片的http链接
+  //     urls: that.data[tagFrom].imageUrls // 需要预览的图片http链接列表
+  //   })
+  // }
 }
 
 /**
