2908feb58c8767f31e849542ccadf393a967e3ce
well505 <316376955@qq.com>
2018-06-12 18:36:49 +0800

    fix:金币明细页面bug修复

--- src/pages/coinDetail/coinDetail.js
+++ src/pages/coinDetail/coinDetail.js
@@ -3,20 +3,18 @@ import { formatTime } from 'utils/util';
 Page({
     data:{
         next_cursor: 0,
-        isRefresh: true,
+        isLoading: true,
     },
-    onLoad: function (options) {
-        this.getCoinList()
+    async onLoad() {
         wx.setNavigationBarTitle({
             title: '金币明细'
         });
+        await this.getCoinList();
+		this.setData({ isLoading: false });
     },
-    async  getCoinList() {
-		const { next_cursor, isRefresh} = this.data;
+    async getCoinList() {
+		const { next_cursor} = this.data;
 		const queryOption = { cursor: next_cursor };
-		if (isRefresh) {
-			wx.showLoading();
-		}
         const data = await api.hei.wallet(queryOption);
         wx.hideLoading();
         
@@ -37,7 +35,7 @@ Page({
     * 页面相关事件处理函数--监听用户下拉动作
     */
     async onPullDownRefresh() {
-        this.setData({ isRefresh: true, next_cursor: 0 });
+        this.setData({ next_cursor: 0 });
         await this.getCoinList();
         wx.stopPullDownRefresh();
     },
