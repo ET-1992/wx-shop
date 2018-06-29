070ba97561bab402ec3e1c64df4aa3da43a50f56
谍 <die@97866.com>
2018-06-29 14:34:51 +0800

    fix: 精简魔方代码

--- src/components/magicImg/magicImg.js
+++ src/components/magicImg/magicImg.js
@@ -6,7 +6,23 @@ Component({
         },
         magicImgData: {
             type: Object,
-            value: {}
+            value: {},
+            observer(newValue) {
+                if (newValue.layout === '2-2') {
+                    newValue.flex = [2, 1];
+                }
+                if (newValue.layout === '2-3') {
+                    newValue.flex = [1, 2];
+                }
+                if (newValue.layout.charAt(newValue.layout.length - 1) === '1' || newValue.layout === '2-2' || newValue.layout === '2-3') {
+                    newValue.defineType = 'oneLine';
+                    newValue.eachNum = Number(newValue.layout.charAt(0));
+                }
+                console.log(newValue, 'fd');
+                this.setData({
+                    magicImgData: newValue
+                });
+            }
         }
     }
 });
