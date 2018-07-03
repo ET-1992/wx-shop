ae55b0a8b10fead02f2b02f8a31e893d72a7b08f
谍 <die@97866.com>
2018-07-03 19:39:33 +0800

    fix: 接入分销申请

--- src/pages/share/shareApply/shareApply.js
+++ src/pages/share/shareApply/shareApply.js
@@ -1,3 +1,5 @@
+import api from 'utils/api';
+
 Page({
     data: {
         title: 'shareApply',
@@ -6,4 +8,26 @@ Page({
     onLoad(parmas) {
         console.log(parmas);
     },
+
+    async joinShareUser() {
+        try {
+            const data = await api.hei.joinShareUser();
+            await wx.showToast({
+                title: '申请成功',
+                icon: 'success',
+                complete: () => {
+                    setTimeout(() => {
+                        wx.redirectTo({
+                            url: '/pages/share/shareCenter/shareCenter'
+                        });
+                    }, 1000);
+                }
+            });
+        } catch (e) {
+            wx.showToast({
+                title: '申请失败',
+                icon: 'none'
+            });
+        }
+    }
 });
