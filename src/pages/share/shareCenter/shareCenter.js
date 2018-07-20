fbf6f2247e4911b0b0d563fa16903d880dd80733
谍 <die@97866.com>
2018-07-20 17:57:57 +0800

    feat: 完善个人中心

--- src/pages/share/shareCenter/shareCenter.js
+++ src/pages/share/shareCenter/shareCenter.js
@@ -4,7 +4,9 @@ Page({
     data: {
         title: 'shareCenter',
         isShowModal: false,
-        loading: true
+        loading: true,
+        member: {},
+        wallet: {}
     },
 
     onLoad(parmas) {
@@ -13,12 +15,50 @@ Page({
     },
     async onShow() {
         const data = await api.hei.shareUserInfo();
+        const { member, wallet } = data;
         console.log(data, 'data');
+        this.setData({
+            member,
+            wallet
+        });
         wx.hideLoading();
     },
     changeModal() {
         this.setData({
             isShowModal: !this.data.isShowModal
+        });
+    },
+    goToShare(e) {
+        console.log(e, '090');
+        const { urldata } = e.currentTarget.dataset;
+        let url = '';
+        switch (urldata) {
+            case 'shareMoney': {
+                const { wallet, member } = this.data;
+                url = `/pages/share/shareMoney/shareMoney?balance=${wallet.balance}&income_pending=${wallet.income_pending}&code=${member.code}`;
+                break;
+            }
+            case 'shareFinishUserInfo': {
+                url = '/pages/share/shareFinishUserInfo/shareFinishUserInfo';
+                break;
+            }
+            case 'shareProductList': {
+                url = '/pages/share/shareProductList/shareProductList';
+                break;
+            }
+            case 'shareDetail': {
+                url = '/pages/share/shareDetail/shareDetail';
+                break;
+            }
+            case 'sharePoster': {
+                url = '/pages/share/sharePoster/sharePoster';
+                break;
+            }
+            default:
+                break;
+        }
+        wx.navigateTo({
+            url
         });
     }
 });
