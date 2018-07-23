01c5642da292f7a14362f4c0332cc480bfcc7549
well505 <316376955@qq.com>
2018-07-23 18:45:09 +0800

    fix:分享订单；列表

--- src/pages/share/shareOrderList/shareOrderList.js
+++ src/pages/share/shareOrderList/shareOrderList.js
@@ -3,23 +3,27 @@ import { getNodeInfo, formatTime } from 'utils/util';
 
 Page({
     data: {
-        title: 'shareOrderList',
+        isLoading: true
     },
 
     onLoad(parmas) {
         console.log(parmas);
+        wx.setNavigationBarTitle({
+            title: '订单列表'
+        });
     },
 
     async onShow() {
         const data = await api.hei.getShareOrderList();
-        console.log(data);
         const { orders } = data;
-        orders.forEach((item) => {
-            item.formatTime = formatTime(new Date(Number(item.modified)) * 1000);
-        });
+        // orders.forEach((item) => {
+        //     item.formatTime = formatTime(new Date(Number(item.modified)) * 1000);
+        // });
 
         this.setData({
-            orders
+            orders,
+            isLoading: false
         });
+        console.log(this.data);
     }
 });
