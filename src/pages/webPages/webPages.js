bd90098dd2940aa0ded09742beaaffe7a9a45598
谍 <die@97866.com>
2018-07-26 17:49:37 +0800

    fix:分销中转

--- src/pages/webPages/webPages.js
+++ src/pages/webPages/webPages.js
@@ -1,10 +1,35 @@
+const app = getApp();
 Page({
     data: {
 
     },
 
     async onLoad(parmas) {
-        const { src } = parmas;
-        this.setData({ src });
+        let { src, scene } = parmas;
+        if (scene) {
+            scene = decodeURIComponent(scene);
+            let query = {};
+            if (scene.indexOf('&') > -1) {
+                const array = scene.split('&');
+                array.forEach((item) => {
+                    const itemArray = item.split('=');
+                    query[itemArray[0]] = itemArray[1];
+                });
+            } else {
+                const sceneArray = scene.split('=');
+                query[sceneArray[0]] = sceneArray[1];
+            }
+            console.log(query, 'query');
+            if (query.afcode) {
+                app.globalData.afcode = query.afcode;
+            }
+            if (query.id) {
+                wx.redirectTo({
+                    url: '/pages/productDetail/productDetail?id=' + query.id
+                });
+            }
+        } else {
+            this.setData({ src });
+        }
     },
 });
