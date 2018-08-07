367dc11a1769049d072af865487b74c13f69514a
well505 <316376955@qq.com>
2018-08-07 19:22:14 +0800

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -71,3 +71,6 @@ export const phoneStyle = {
 };
 /* 商品列表排版风格 */
 export const PRODUCT_LAYOUT_STYLE = ['topImage', 'leftImage', 'rightImage', 'articleImage', 'shareProduct'];
+
+/* 模板 */
+export const TPL_STYLE = ['one', 'two', 'three', 'four'];
