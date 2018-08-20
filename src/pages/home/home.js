05baa9a9832310005b87e0e0cdaa0d334e5c4e53
well505 <316376955@qq.com>
2018-08-20 10:36:21 +0800

    fix:首页滚动公告

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -38,12 +38,6 @@ Page({
         hasMask: false,
         isShowConsole: false,
         swiperCurrent: 0,
-
-        msgList: [
-            { url: 'url', title: '公告：多地首套房贷利率上浮 热点城市渐迎零折扣时代' },
-            { url: 'url', title: '公告：悦如公寓三周年生日趴邀你免费吃喝欢唱' },
-            { url: 'url', title: '公告：你想和一群有志青年一起过生日嘛？' }
-        ]
     },
 
     swiperChange(e) {
