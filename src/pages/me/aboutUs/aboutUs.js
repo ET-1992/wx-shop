d32b0a5c4870cc9a265ad210cb668b1e44247792
well505 <316376955@qq.com>
2018-08-20 18:29:34 +0800

    feat：关于我们

---
+++ src/pages/me/aboutUs/aboutUs.js
@@ -0,0 +1,39 @@
+const app = getApp();
+Page({
+
+    /**
+   * 页面的初始数据
+   */
+    data: {
+        showBtn: false
+    },
+
+    /**
+   * 生命周期函数--监听页面加载
+   */
+    onLoad(params) {
+        const { image_url, phone } = params;
+        const { themeColor } = app.globalData;
+        this.setData({
+            image_url,
+            phone,
+            themeColor
+        });
+        console.log(this.data);
+    },
+
+    /* 图片加载完毕回调 */
+    isLoaded(e) {
+        console.log(e.detail);
+        this.setData({
+            showBtn: true
+        });
+    },
+
+    call() {
+        wx.makePhoneCall({
+            phoneNumber: this.data.phone
+        });
+    }
+
+});
