c615e19e41594c941df04e7fe71d7467d973efb1
well505 <well@97866.com>
2018-11-20 18:40:56 +0800

    fix:功能面板组件

---
+++ src/pages/me/infos/index.js
@@ -0,0 +1,65 @@
+import { getUserInfo, auth } from 'utils/util';
+import { chooseAddress, showToast } from 'utils/wxp';
+import { ADDRESS_KEY } from 'constants/index';
+
+Component({
+    properties: {
+        infosComponentData: {
+            type: Object,
+            value: {},
+            observer(newValue) {
+                this.setData({
+                    ...newValue
+                });
+            }
+        }
+    },
+    methods: {
+        async onAddress() {
+            const res = await auth({
+                scope: 'scope.address',
+                ctx: this
+            });
+            if (res) {
+                const addressRes = await chooseAddress();
+                wx.setStorageSync(ADDRESS_KEY, addressRes);
+            }
+        },
+        /* 我的管家 */
+        async openManager() {
+            const address  = wx.getStorageSync('address');
+            if (!address) {
+                const res = await auth({
+                    scope: 'scope.address',
+                    ctx: this
+                });
+                if (res) {
+                    const addressRes = await chooseAddress();
+                    wx.setStorageSync(ADDRESS_KEY, addressRes);
+                }
+            } else {
+                this.setData({
+                    openManager: true
+                });
+            }
+        },
+        redirectToShareCenter() {
+            const user = getUserInfo();
+            if (user) {
+                wx.navigateTo({ url: '/pages/affiliate/affiliateCenter/affiliateCenter' });
+            }
+        },
+        showToast() {
+            showToast({
+                title: '审核中',
+                icon: 'none'
+            });
+        },
+        /* 拨打售后电话 */
+        call(e) {
+            wx.makePhoneCall({
+                phoneNumber: e.currentTarget.dataset.phone
+            });
+        },
+    }
+});
