6644c49c8180704c8672f6e9cd9b6be54310f0fe
谍 <die@97866.com>
2018-11-21 11:07:49 +0800

    fix: 创建订单页商品金额

--- src/pages/orderCreate/orderInfo/index.js
+++ src/pages/orderCreate/orderInfo/index.js
@@ -28,7 +28,8 @@ Component({
             observer(newValue) {
                 this.setData({
                     postageDispaly: newValue.postage ? Number(newValue.postage).toFixed(2) : '0.00',
-                    couponFeeDispaly: newValue.coupon_reduce_fee ? Number(newValue.coupon_reduce_fee).toFixed(2) : '0.00'
+                    couponFeeDispaly: newValue.coupon_reduce_fee ? Number(newValue.coupon_reduce_fee).toFixed(2) : '0.00',
+                    totalPriceDispaly: newValue.item_amount ? Number(newValue.item_amount).toFixed(2) : '0.00'
                 });
             }
         },
