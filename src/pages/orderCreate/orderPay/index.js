33412912e0d20b441ebebf63e479c1315ab0e0e7
well505 <well@97866.com>
2018-12-03 16:52:51 +0800

    fix: bug修复

--- src/pages/orderCreate/orderPay/index.js
+++ src/pages/orderCreate/orderPay/index.js
@@ -30,18 +30,21 @@ Component({
         onPay(e) {
             const { formId } = e.detail;
             const { crowd, finalPay } = this.data;
-
-            finalPay > 0
-                ?
-                this.triggerEvent('onpay', { formId, crowd }, { bubbles: true })
-                :
-                wx.showModal({
-                    title: '温馨提示',
-                    content: '订单金额为0元',
-                    showCancel: false,
-                    confirmText: '确定',
-                    confirmColor: '#3CC51F',
-                });
+            if (crowd) {
+                finalPay > 0
+                    ?
+                    this.triggerEvent('onpay', { formId, crowd }, { bubbles: true })
+                    :
+                    wx.showModal({
+                        title: '温馨提示',
+                        content: '订单金额为0元',
+                        showCancel: false,
+                        confirmText: '确定',
+                        confirmColor: '#3CC51F',
+                    });
+            } else {
+                this.triggerEvent('onpay', { formId }, { bubbles: true });
+            }
         }
     }
 });
