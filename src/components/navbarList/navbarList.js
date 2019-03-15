c43e16947646f4143e7e4ec0d2b0fd03c984be72
well505 <well@97866.com>
2019-03-15 17:13:02 +0800

    feat:navbar组件

---
+++ src/components/navbarList/navbarList.js
@@ -0,0 +1,35 @@
+Component({
+    properties: {
+        navbarListData: {
+            type: Array,
+            value: [],
+        },
+        themeColor: {
+            type: Object,
+            value: {}
+        },
+        template: {
+            type: String,
+            value: 'default'
+        },
+        activeIndex: {
+            type: Number,
+            value: 0,
+            observer(newvalue) {
+                this.setData({
+                    toView: `tab${newvalue}`
+                });
+            }
+        }
+    },
+    methods: {
+        changeActive(e) {
+            const res = e.currentTarget.dataset;
+            let data = { activeIndex: res.index };
+            data.toView = `tab${res.index}`;
+            this.setData(data);
+            this.triggerEvent('changeNavbarList', res, { bubbles: true });
+        }
+    }
+});
+
