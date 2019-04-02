fae270fe14de9b5aed5c314e4cfa9b689b6a0ea4
well <well@97866.com>
2019-04-02 14:51:49 +0800

    fix:bug 修复

--- src/pages/redpacket/redpacket.js
+++ src/pages/redpacket/redpacket.js
@@ -1,47 +1,49 @@
-import { USER_KEY, CONFIG } from 'constants/index';
 import api from 'utils/api';
-import { showToast } from 'utils/wxp';
+import proxy from 'utils/wxProxy';
 import { autoNavigate } from 'utils/util';
 const app = getApp();
 Page({
     data: {
-        title: 'redpacket',
         redpacket: {},
         products: [],
         hasRecived: false,
-        isFinished: false
+        isFinished: false,
+        isLoading: true
     },
-
-    async loadRepacket() {
+    async onLoad() {
         const { isIphoneX } = app.systemInfo;
+        const { themeColor } = app.globalData;
+        const data = await api.hei.config();
+        const { style_type: tplStyle = 'default' } = data.config;
+        this.setData({
+            tplStyle,
+            isIphoneX,
+            themeColor,
+            globalData: app.globalData,
+            ...data
+        });
+        await this.loadRepacket();
+    },
+    async loadRepacket() {
         const { id } = this.options;
         let goldNumer = 0;
-        const { products = [], received_redpacket, shared_redpacket } = await api.hei.fetchRedpacket({ packet_no: id });
-        // const { stock_qty } = shared_redpacket;
+        const { products = [], received_redpacket } = await api.hei.fetchRedpacket({ packet_no: id });
         if (received_redpacket) {
             received_redpacket.item.reduceValue = Number(received_redpacket.item.reduce_cost);
             goldNumer = parseInt(received_redpacket.item.amount * 100, 10);
         }
-        const { themeColor } = app.globalData;
-        const config = wx.getStorageSync(CONFIG);
-        const { style_type: tplStyle = 'default' } = config;
-
         this.setData({
             products,
             redpacket: received_redpacket,
-            goldNumer: goldNumer,
+            goldNumer,
             hasRecived: !!received_redpacket,
-            themeColor,
-            tplStyle,
-            isIphoneX,
-            config,
-            globalData: app.globalData
+            isLoading: false
         });
         console.log(this.data);
     },
 
+    /* 领取 */
     async onRecive() {
-        // wx.showLoading();
         const { id } = this.options;
         let goldNumer = 0;
         try {
@@ -51,18 +53,17 @@ Page({
                 received_redpacket.item.reduceValue = Number(received_redpacket.item.reduce_cost);
                 goldNumer = parseInt(received_redpacket.item.amount * 100, 10);
             }
-            await showToast({ icon: 'success', title: '领取成功' });
+            await proxy.showToast({ icon: 'success', title: '领取成功' });
             this.setData({
                 hasRecived: true,
                 isFinished: false,
                 products,
                 redpacket: received_redpacket,
-                goldNumer: goldNumer
+                goldNumer,
             });
-
         } catch (e) {
             if (e && e.errMsg) {
-                await showToast({ title: e.errMsg, icon: 'none' });
+                await proxy.showToast({ title: e.errMsg, icon: 'none' });
                 this.setData({
                     hasRecived: true,
                     isFinished: true
@@ -71,28 +72,18 @@ Page({
         }
     },
 
-    async onShow() {
-        await this.loadRepacket();
-    },
-
+    /* 立即使用 */
     onUse() {
         autoNavigate('/pages/home/home');
     },
 
-    // async reLoad() {
-    // 	await this.loadRepacket();
-    // },
-
-    onShareAppMessage({ target }) {
-        const { user = {}} = this.data.redpacket;
-        const currentUser = wx.getStorageSync(USER_KEY);
+    onShareAppMessage() {
+        const { current_user, config } = this.data;
         const { id } = this.options;
-        const name = target === 'button' ? currentUser.nickname : user.nickname;
-
         return {
-            title: `好友${name}给你发来了一个红包，快去领取吧`,
+            title: `好友${current_user.nickname}给你发来了一个红包，快去领取吧`,
             path: `/pages/redpacket/redpacket?id=${id}`,
-            imageUrl: `${this.data.config.cdn_host}/shop/redpacketShare.jpg`
+            imageUrl: `${config.cdn_host}/shop/redpacketShare.jpg`
         };
     },
 
