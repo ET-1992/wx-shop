9f51e07c0e47a428f460d1eaaee3890265a37473
MAO <heyhey@97886.com>
2019-06-25 12:35:41 +0800

    feat: 添加浏览用户页面

---
+++ src/pages/affiliate/affiliateVisitor/affiliateVisitor.js
@@ -0,0 +1,12 @@
+import { go } from 'utils/util';
+Page({
+    data: {
+        title: 'affiliateVisitor'
+    },
+    go() {
+        go();
+    },
+    onLoad(params) {
+        console.log(params);
+    }
+});
