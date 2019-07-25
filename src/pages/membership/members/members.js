8ce6c2ccdd1e9a7e49ab6c4ff81d2c85773b8772
MAO <heyhey@97866.comm>
2019-07-25 17:39:30 +0800

    feat: 会员中心添加会员优惠券和会员专属商品banner

--- src/pages/membership/members/members.js
+++ src/pages/membership/members/members.js
@@ -10,6 +10,7 @@ import {
 import {
     wxPay
 } from 'utils/pageShare';
+import { showToast, showModal } from 'utils/wxp';
 import api from 'utils/api';
 const app = getApp();
 
@@ -21,7 +22,8 @@ Page({
         rechargeModal: false,  // 会员充值弹窗
         consoleTime: 0,
         updateAgainUserForInvalid: false, // 是否已更新头像
-        ruleData: {} // 会员规则 word image_url
+        ruleData: {}, // 会员规则 word image_url
+        memberCouponList: {} // 会员优惠券
     },
 
     onLoad(params) {
@@ -34,6 +36,7 @@ Page({
         app.log('页面onShow');
         this.getMemberHome();
         this.initPage();
+        this.getCoupon();
     },
 
     // 初始页面配置
@@ -41,10 +44,8 @@ Page({
         const {
             themeColor
         } = app.globalData;
-
         // 获取全局店铺配置信息
         const config = wx.getStorageSync(CONFIG);
-
         // 获取开启储值卡后充值金额数组
         if (config.store_card_enable) {
             const recharge = await api.hei.rechargePrice();
@@ -53,7 +54,6 @@ Page({
                 rechargeArray: recharge.data
             });
         }
-
         this.setData({
             isLoading: false,
             themeColor,
@@ -86,6 +86,66 @@ Page({
                 user
             }, this.onShow);
             this.updateAgainUserForInvalid = true;
+        }
+    },
+
+    // 获取会员优惠券
+    async getCoupon() {
+        const MEMBERCOUPON = '1';
+        const CouponList = await api.hei.fetchCouponList({
+            membership: MEMBERCOUPON
+        });
+        this.setData({
+            memberCouponList: CouponList.coupons
+        });
+    },
+
+    // 发送模板消息
+    async submitFormId(e) {
+        const data = await api.hei.submitFormId({
+            form_id: e.detail.formId,
+        });
+        console.log(data);
+    },
+
+    // 点击优惠券
+    async onCouponClick(ev) {
+        const { id, index, status, title } = ev.currentTarget.dataset;
+        if (Number(status) === 2) {
+            // 立即领取
+            await this.onReceiveCoupon(id, index);
+        } else if (Number(status) === 4) {
+            // 立即使用
+            wx.navigateTo({
+                url: `/pages/couponProducts/couponProducts?couponId=${id}&couponTitle=${title}`,
+            });
+        } else { return }
+    },
+
+    // 领取优惠券
+    async onReceiveCoupon(id, index) {
+        try {
+            const data = await api.hei.receiveCoupon({
+                coupon_id: id,
+            });
+            console.log(data);
+            const { errcode } = data;
+
+            if (!errcode) {
+                showToast({ title: '领取成功' });
+                const updateData = {};
+                const key = `coupons[${index}].status`;
+                updateData[key] = 4;
+                console.log(updateData);
+                this.setData(updateData);
+            }
+        }
+        catch (err) {
+            await showModal({
+                title: '温馨提示',
+                content: err.errMsg,
+                showCancel: false,
+            });
         }
     },
 
