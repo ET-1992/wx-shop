f59cc85baaeb7a522790a0aea28202c05658666a
well <well@97866.com>
2019-10-22 12:14:59 +0800

    fix: 文章评论修复

--- src/pages/commentList/commentList.js
+++ src/pages/commentList/commentList.js
@@ -1,6 +1,5 @@
 import api from 'utils/api';
 import { getAgainUserForInvalid, getUserInfo } from 'utils/util';
-import { showModal, showToast } from 'utils/wxp';
 
 const app = getApp();
 
@@ -29,7 +28,7 @@ Page({
     /**
 	 * 生命周期函数--监听页面加载
 	 */
-    onLoad: function (options) {
+    onLoad(options) {
         const { globalData: { themeColor }, systemInfo } = app;
         const updateData = { themeColor, systemInfo };
         this.setData(updateData);
@@ -72,28 +71,19 @@ Page({
     },
 
     async onUserInfo(ev) {
-        if (this.data.user) {
+        const { encryptedData, iv } = ev.detail;
+        if (encryptedData && iv) {
+            await getAgainUserForInvalid({ encryptedData, iv });
             await this.submitComment();
         }
         else {
-            const { encryptedData, iv } = ev.detail;
-            if (encryptedData && iv) {
-                const user = await getAgainUserForInvalid({ encryptedData, iv });
-                await showToast({
-                    title: '授权成功',
-                    icon: 'success', // 仅支持success或者loading
-                    duration: 1000,
-                });
-                this.setData({ user });
-            }
-            else {
-                showModal({
-                    title: '温馨提示',
-                    content: '需授权后操作',
-                    showCancel: false,
-                });
-            }
+            wx.showModal({
+                title: '温馨提示',
+                content: '需授权后操作',
+                showCancel: false,
+            });
         }
+
     },
 
     async formSubmit(ev) {
@@ -104,93 +94,93 @@ Page({
     },
 
     async submitComment() {
-        const { formId, text, reply_to } = this.data;
-        if (text === '' || isSubmiting) return;
+        try {
+            const { formId, text, reply_to, topic, id } = this.data;
+            if (text === '' || isSubmiting) {
+                wx.showToast({
+                    title: '请输入内容',
+                    icon: 'none',
+                    mask: true
+                });
+                return;
+            }
 
-        isSubmiting = true;
+            isSubmiting = true;
 
-        let args = {
-            text,
-            reply_to,
-            form_id: formId,
-        };
+            let args = {
+                text,
+                reply_to,
+                form_id: formId,
+                post_id: id
+            };
 
-        let res;
+            const { reply } = await api.hei.createReply(args);
 
-        args.post_id = this.data.id;
-        res = await api.hei.createReply({ ...args });
+            topic.reply_count = topic.reply_count + 1;
+            topic.replies = [].concat(topic.replies, reply);
 
-        if (res.errcode === 0) {
             this.setData({
+                topic,
                 text: '',
                 reply_to: 0,
                 placeholder: '发布评论',
                 reply_focus: false,
             });
-            let topic = this.data.topic;
-            topic.reply_count = topic.reply_count + 1;
-            topic.replies = [].concat(topic.replies, res.reply);
-            this.setData({
-                topic,
-            });
             isSubmiting = false;
+        } catch (e) {
+            wx.showModal({
+                title: '温馨提示',
+                content: e.errMsg,
+                showCancel: false
+            });
         }
     },
 
-    async deleteReply(comment_id) {
-        const res = await api.hei.deleteReply({
-            id: comment_id,
-        });
-        if (res.errcode === 0) {
-            let topic = this.data.topic;
-            topic.reply_count = topic.reply_count - 1;
-            topic.replies = topic.replies.filter((item) => item.id !== comment_id);
-            this.setData({ topic });
-            isSubmiting = false;
-        }
-    },
+    // async deleteReply(comment_id) {
+    //     try {
+    //         const { topic } = this.data;
+    //         await api.hei.deleteReply({ id: comment_id, });
+    //         topic.reply_count = topic.reply_count - 1;
+    //         topic.replies = topic.replies.filter((item) => item.id !== comment_id);
+    //         this.setData({ topic });
+    //         isSubmiting = false;
+    //     } catch (e) {
+    //         wx.showModal({
+    //             title: '温馨提示',
+    //             content: e.errMsg,
+    //             showCancel: false
+    //         });
+    //     }
+    // },
 
     async replyTo(e) {
-        const self = this;
-        const comment_id = e.currentTarget.dataset.id;
-        const username = e.currentTarget.dataset.user;
-        const openid = e.currentTarget.dataset.openid;
-        let userForm;
-        let user = this.data.user;
+        const { iv, encryptedData } = e.detail;
+        const { id, nickname, openid } = e.currentTarget.dataset;
+        const { user } = this.data;
         if (!user || !user.openid) {
-            console.log(user);
-            userForm = await api.user.login();
-            user = await api.hei.getProfile(userForm);
-            if (user.user) {
-                user = user.user;
-            }
-            else {
-                return;
-            }
+            const user = await getAgainUserForInvalid({ encryptedData, iv });
             this.setData({ user });
         }
         if (openid === user.openid) {
-            /* console.log('删除评论');
-            wx.showActionSheet({
-                itemList: ['删除评论'],
-                itemColor: '#e74c3c',
-                success: function (res) {
-                    if (res.tapIndex === 0) {
-                        self.deleteReply(comment_id, self.data.type);
-                    }
-                },
-            }); */
-        }
-        else {
-            console.log('其他人评论');
+            // 自己评论则删除
+            console.log('删除评论');
+            // let that = this;
+            // wx.showActionSheet({
+            //     itemList: ['删除评论'],
+            //     itemColor: '#e74c3c',
+            //     success: (res) => {
+            //         if (res.tapIndex === 0) {
+            //             that.deleteReply(id);
+            //         }
+            //     }
+            // });
+        } else {
+            // 其他人评论则回复
             isFocusing = true;
-
-            // 3.如果是其他人评论则回复
-            console.log('reply_to', isFocusing);
-            self.setData({
-                reply_to: comment_id,
-                placeholder: '回复' + username + ':',
-                reply_focus: true,
+            this.setData({
+                reply_to: id,
+                placeholder: '回复' + nickname + ':',
+                reply_focus: true
             });
         }
     },
