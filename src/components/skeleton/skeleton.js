5451949ee0e9185837f5edf33d190fac42b69e3f
MAOJIE <heyhey@97866.com>
2019-11-27 19:00:26 +0800

    feat: 商品详情页面添加骨架屏

---
+++ src/components/skeleton/skeleton.js
@@ -0,0 +1,13 @@
+Component({
+    properties: {
+        title: {
+            type: String,
+            value: 'skeleton Component',
+        },
+        isIphoneX: {
+            type: Boolean,
+            value: false,
+        }
+    }
+});
+
