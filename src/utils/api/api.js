120f792ff85b9407ea533fe2406685adcfc10841
well <well@97866.com>
2020-04-21 15:43:16 +0800

    fix: 地址接口

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -514,5 +514,29 @@ export const apis = {
     newHome: {
         path: 'api/module/page.json',
         method: 'GET'
-    }
+    },
+    getReceiverList: {
+        path: 'api/mag.platform_user.receiver.list.json',
+        isForceToken: true
+    },
+    addReceiverInfo: {
+        path: 'api/mag.platform_user.receiver.add.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    getReceiverInfo: {
+        path: 'api/mag.platform_user.receiver.get.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    updateReceiverInfo: {
+        path: 'api/mag.platform_user.receiver.update.json',
+        method: 'POST',
+        isForceToken: true
+    },
+    deleteReceiverInfo: {
+        path: 'api/mag.platform_user.receiver.delete.json',
+        method: 'POST',
+        isForceToken: true
+    },
 };
