2f9a0dbe606daa2ed7b4b1c244e07a47a7dc0d65
MAOJIE <heyhey@97866.com>
2020-06-05 17:36:10 +0800

    feat: 将微信定位方法代替联想地址表单，隐藏地区表单，重构代码结构

--- src/pages/addressEdit/addressEdit.js
+++ src/pages/addressEdit/addressEdit.js
@@ -40,10 +40,13 @@ Page({
         showAreaPanel: false,  // 省市区弹出框
         areacode: '',  // 省市区编码值
         originForm: '',  // 后端返回的地址表单
-        mapList: [],  // 腾讯地图搜索结果列表
+        mapList: [],  // 腾讯地图关键词搜索结果列表
         mapListPanel: false,  // 地图展示面板
         selectedMap: {},  // 地图选中项
         config: {},
+        qqAddress: {},  // 腾讯地图逆地址解析结果
+        qqLocation: {},  // 腾讯地图地址解析结果
+        locationObj: {},  // 地址坐标对象
     },
 
     onLoad(params) {
@@ -64,19 +67,19 @@ Page({
         wx.setNavigationBarTitle({
             title: typeToTitle[type],
         });
-        if (config.offline_store_enable) {
-            this.addFormItem();
-        }
+        // if (config.offline_store_enable) {
+        this.addFormItem();
+        // }
         if (type === 'update') {
-            this.getAddressInfo();
+            this.getUpdateAddressInfo();
         } else if (type === 'orderEdit') {
-            this.renderOrderEdit();
+            this.getOrderAddress();
         } else {
             this.setData({
                 isLoading: false,
             });
         }
-        this.getAreaData();
+        // this.getAreaData();
     },
 
     // 表单添加门牌号
@@ -86,6 +89,7 @@ Page({
         let formItem = { key: 'houseNumber', value: '', label: '门牌号' };
         formBackEnd['houseNumber'] = 'room';
         formWechat['houseNumber'] = 'room';
+        // 在详细地址后面插入
         form.splice(index, 0, formItem);
         this.setData({
             form,
@@ -95,19 +99,20 @@ Page({
     },
 
     // 获取具体地址
-    async getAddressInfo() {
+    async getUpdateAddressInfo() {
         this.setData({ isLoading: true });
         let { id } = this.data;
         let data = await api.hei.getReceiverInfo({ receiver_id: id });
         this.setData({
             originForm: data.receiver,
         });
-        this.showFormData();
+        this.showUpdateAddress();
     },
 
     // 在页面上展示具体地址
-    showFormData() {
+    showUpdateAddress() {
         let { form, originForm, formBackEnd } = this.data;
+        let { latitude, longtitude } = originForm;
         // 遍历对应字段表
         for (let [k, v] of Object.entries(formBackEnd)) {
             // 遍历表单数组
@@ -122,6 +127,7 @@ Page({
         }
         let areacode = originForm.receiver_areacode;
         this.setData({
+            locationObj: { latitude, longitude: longtitude },
             form,
             areacode,
             isLoading: false,
@@ -129,13 +135,13 @@ Page({
     },
 
     // 导入多门店两种地址
-    renderOrderEdit() {
+    getOrderAddress() {
         let location = wx.getStorageSync(LOCATION_KEY) || false;
         let address = wx.getStorageSync(ADDRESS_KEY) || {};
         if (location) {
-            this.renderLocation();
+            this.showOrderLocation();
         } else if (address.userName) {
-            this.renderAddress();
+            this.showOrderAddress();
         }
         this.setData({
             isLoading: false,
@@ -143,25 +149,28 @@ Page({
 
     },
 
-    // 导入多门店定位地址
-    renderLocation() {
+    // 展示多门店定位地址
+    showOrderLocation() {
         let { form } = this.data;
         let locationObj = wx.getStorageSync(LOCATION_KEY) || {};
-        let { address, ad_info } = locationObj;
-        let { adcode, province, city, district } = ad_info || {};
+        let { address, location } = locationObj;
+        let { lat, lng } = location;
+        // let { adcode, province, city, district } = ad_info || {};
         // 表单地址选项
-        form[2].value = [province, city, district];
-        form[2].areacode = adcode;
+        let index = form.findIndex(item => item.key === 'address');
+        // form[index].value = [province, city, district];
+        // form[index].areacode = adcode;
         // 表单详细地址选项
-        form[3].value = address;
+        form[index + 1].value = address;
         this.setData({
-            areacode: adcode,
+            // areacode: adcode,
             form,
+            locationObj: { latitude: lat, longitude: lng }
         });
     },
 
-    // 导入多门店收货地址
-    renderAddress() {
+    // 展示多门店收货地址
+    showOrderAddress() {
         let { form, formWechat } = this.data;
         let address = wx.getStorageSync(ADDRESS_KEY) || {};
         console.log('formWechat', formWechat);
@@ -177,9 +186,11 @@ Page({
             }
         }
         let areacode = address.nationalCode;
+        let { latitude, longitude } = address;
         this.setData({
             form,
             areacode,
+            locationObj: { latitude, longitude }
         });
     },
 
@@ -196,12 +207,7 @@ Page({
         });
     },
 
-    // 收起展示面板
-    onCloseAddress() {
-        this.setData({ mapListPanel: false });
-    },
-
-    // 详细地址输入框变化
+    // 联想地址输入框变化
     onAddressInfo: debounce(function(e) {
         let { detail } = e;
         if (detail) {
@@ -211,7 +217,12 @@ Page({
         }
     }, 400, true),
 
-    // 搜索地址请求
+    // 收起联想地址弹出框
+    onCloseAddress() {
+        this.setData({ mapListPanel: false });
+    },
+
+    // 联想地址请求
     async searchAddress(value) {
         let { form } = this.data;
         // 搜索地区
@@ -244,7 +255,7 @@ Page({
         });
     },
 
-    // 点击搜索地图单项
+    // 选择联想地址框
     onSelectAddress(e) {
         let { mapList } = this.data;
         let { id } = e.currentTarget.dataset;
@@ -259,7 +270,7 @@ Page({
         this.updateAddressForm();
     },
 
-    // 点击详细地址更改表单数据
+    // 触发联想地址结果
     updateAddressForm() {
         let { selectedMap, form } = this.data;
         let { address, ad_info = {}} = selectedMap;
@@ -274,169 +285,6 @@ Page({
             form,
         });
         console.log('selectedMap', selectedMap);
-    },
-
-    // 保存表单
-    async onConfirmForm() {
-        let { type } = this.data;
-        try {
-            this.checkForm();
-            if (type === 'orderEdit') {
-                // 多门店地址
-                await this.saveStoreAddress();
-            } else {
-                // 地址列表
-                await this.saveListAddress();
-            }
-        } catch (error) {
-            wx.showModal({
-                title: '温馨提示',
-                content: error.message || error.errMsg || '提交失败',
-                showCancel: false,
-            });
-        }
-    },
-
-    // 多门店确定地址
-    async saveStoreAddress() {
-        wx.showLoading({
-            title: '加载中',
-        });
-        try {
-            let finalForm = await this.saveStorageAddrss();
-            app.event.emit('setAddressListEvent', finalForm);
-            wx.showModal({
-                title: '温馨提示',
-                content: '地址编辑成功',
-                showCancel: false,
-                success: () => wx.navigateBack(),
-            });
-        } catch (err) {
-            throw (err);
-        } finally {
-            wx.hideLoading();
-        }
-    },
-
-    // 保存多门店编辑
-    async saveStorageAddrss() {
-        let location = wx.getStorageSync(LOCATION_KEY) || false;
-        let { form, formWechat, areacode } = this.data;
-        let finalForm = {};
-        // 遍历对应字段表
-        for (let [k, v] of Object.entries(formWechat)) {
-            // 遍历表单数组
-            for (let elem of form.values()) {
-                if (elem.key === k && Array.isArray(v)) {
-                    // 地址
-                    v.forEach((item, index) => { finalForm[item] = elem.value[index] });
-                } else if (elem.key === k && !Array.isArray(v)) {
-                    finalForm[v] = elem.value;
-                }
-            }
-        }
-        let locationObj = await this.parseAddress(form) || {};
-        let { latitude, longitude } = locationObj;
-        // 添加经纬度
-        finalForm.latitude = latitude;
-        finalForm.longitude = longitude;
-        this.checkOrderAddress({ latitude, longitude });
-        if (location) {
-            // 定位信息完善为地址信息
-            wx.setStorageSync(LOCATION_KEY, null);
-        }
-        wx.setStorageSync(ADDRESS_KEY, finalForm);
-        return finalForm;
-    },
-
-    // 收货地址解析
-    async parseAddress(form) {
-        let address = form[2].value;
-        let addressInfo = form[3].value;
-        let addressStr = [...address, addressInfo].join('');
-        let cityName = address[1];
-        let data = {
-            key: 'XHSBZ-OOU6P-DHDDK-LEC5P-3CBJ6-VXF5H',
-            address: addressStr,
-            region: cityName,
-        };
-        let url = 'https://apis.map.qq.com/ws/geocoder/v1';
-        try {
-            let res = await proxy.request({
-                url,
-                data,
-            });
-            console.log('收货地址解析结果：', res);
-            let lat = '',
-                lng = '';
-            if (res.data && res.data.status === 0) {
-                ({ lat, lng } = res.data.result.location);
-            }
-            return {
-                latitude: lat,
-                longitude: lng,
-            };
-        } catch (error) {
-            console.log('地址解析错误', error);
-            throw new Error('找不到该地址的地理位置');
-        }
-    },
-
-    // 地址列表确定地址
-    async saveListAddress() {
-        wx.showLoading({
-            title: '加载中',
-        });
-        let { type = 'update', typeToTitle } = this.data;
-        let modalContent = `${typeToTitle[type]}成功`;
-        try {
-            await this.submitListAddress();
-            wx.showModal({
-                title: '温馨提示',
-                content: modalContent,
-                showCancel: false,
-                success: () => wx.navigateBack(),
-            });
-        } catch (err) {
-            throw (err);
-        } finally {
-            wx.hideLoading();
-        }
-
-    },
-
-    // 向后端提交地址请求
-    async submitListAddress() {
-        let { form, formBackEnd, areacode, type, id } = this.data;
-        let finalForm = {};
-        // 遍历对应字段表
-        for (let [k, v] of Object.entries(formBackEnd)) {
-            // 遍历表单数组
-            for (let elem of form.values()) {
-                if (elem.key === k && Array.isArray(v)) {
-                    // 地址
-                    v.forEach((item, index) => { finalForm[item] = elem.value[index] });
-                } else if (elem.key === k && !Array.isArray(v)) {
-                    finalForm[v] = elem.value;
-                }
-            }
-        }
-        let apiMethod = 'addReceiverInfo';
-        let { latitude, longitude } = await this.parseAddress(form) || {};
-        // 省市区编号和地址ID
-        finalForm.receiver_areacode = areacode;
-        if (type === 'update') {
-            // 地址编辑操作
-            finalForm.receiver_id = id;
-            apiMethod = 'updateReceiverInfo';
-        }
-        // 国家和是否默认地址
-        finalForm.receiver_country = '';
-        finalForm.receiver_default = 0;
-        // 添加经纬度
-        finalForm.latitude = latitude;
-        finalForm.longtitude = longitude;  // 注意经度字段
-        await api.hei[apiMethod](finalForm);
     },
 
     // 删除地址表单
@@ -455,23 +303,200 @@ Page({
                 wx.navigateBack();
             }
         });
+    },
+
+    // 保存地址表单
+    async onConfirmForm() {
+        let { type } = this.data;
+        wx.showLoading({
+            title: '加载中',
+        });
+        try {
+            await this.parseLocationData();
+            this.checkAddressForm();
+            if (type === 'orderEdit') {
+                // 确认多门店地址表单
+                await this.confirmStoreAddress();
+            } else {
+                // 确定收货地址表单
+                await this.confirmListAddress();
+            }
+        } catch (error) {
+            wx.showModal({
+                title: '温馨提示',
+                content: error.message || error.errMsg || '提交失败',
+                showCancel: false,
+            });
+        } finally {
+            wx.hideLoading();
+        }
+    },
+
+    // 确认多门店地址表单
+    async confirmStoreAddress() {
+        let finalForm = await this.saveStoreAddress();
+        app.event.emit('setAddressListEvent', finalForm);
+        wx.showModal({
+            title: '温馨提示',
+            content: '地址编辑成功',
+            showCancel: false,
+            success: () => wx.navigateBack(),
+        });
+    },
+
+    // 保存多门店地址
+    async saveStoreAddress() {
+        let location = wx.getStorageSync(LOCATION_KEY) || false;
+        let { form, formWechat, locationObj, qqLocation } = this.data;
+        let { ad_info } = qqLocation;
+        let { province, city, district } = ad_info;
+        let index = form.findIndex(item => item.key === 'address');
+        form[index].value = [province, city, district];
+        let finalForm = {};
+        // 遍历对应字段表
+        for (let [k, v] of Object.entries(formWechat)) {
+            // 遍历表单数组
+            for (let elem of form.values()) {
+                if (elem.key === k && Array.isArray(v)) {
+                    // 地址
+                    v.forEach((item, index) => { finalForm[item] = elem.value[index] });
+                } else if (elem.key === k && !Array.isArray(v)) {
+                    finalForm[v] = elem.value;
+                }
+            }
+        }
+        let { latitude, longitude } = locationObj;
+        // 添加经纬度
+        finalForm.latitude = latitude;
+        finalForm.longitude = longitude;
+        this.checkOrderAddress({ latitude, longitude });
+        if (location) {
+            // 定位信息完善为地址信息
+            wx.setStorageSync(LOCATION_KEY, null);
+        }
+        wx.setStorageSync(ADDRESS_KEY, finalForm);
+        return finalForm;
+    },
+
+    // 地址解析
+    async parseAddressData() {
+        let { form } = this.data;
+        let address = form[2].value || [];
+        let addressInfo = form[3].value;
+        let addressStr = [...address, addressInfo].join('');
+        let cityName = address[1];
+        let data = {
+            key: 'XHSBZ-OOU6P-DHDDK-LEC5P-3CBJ6-VXF5H',
+            address: addressStr,
+            region: cityName,
+        };
+        let url = 'https://apis.map.qq.com/ws/geocoder/v1';
+        try {
+            let res = await proxy.request({ url, data });
+            console.log('地址解析结果：', res);
+            let resObj = {};
+            if (res.data && res.data.status === 0) {
+                ({ result: resObj } = res.data);
+            }
+            this.setData({
+                qqAddress: resObj,
+            });
+        } catch (error) {
+            console.log('地址解析错误', error);
+            throw new Error('找不到该地址的地理位置');
+        }
+    },
+
+    // 地址逆解析
+    async parseLocationData() {
+        let { latitude, longitude } = this.data.locationObj;
+        let data = {
+            key: 'XHSBZ-OOU6P-DHDDK-LEC5P-3CBJ6-VXF5H',
+            location: `${latitude},${longitude}`
+        };
+        let url = 'https://apis.map.qq.com/ws/geocoder/v1';
+        try {
+            let res = await proxy.request({ url, data });
+            console.log('经纬度解析的结果：', res);
+            if (res.data && res.data.status === 0) {
+                let { result = {}} = res.data;
+                this.setData({
+                    qqLocation: result,
+                });
+            }
+        } catch (error) {
+            console.log('经纬度解析的错误', error);
+            throw new Error('无法定位该地理位置');
+        }
+    },
+
+    // 确定收货地址表单
+    async confirmListAddress() {
+        let { type = 'update', typeToTitle } = this.data;
+        let modalContent = `${typeToTitle[type]}成功`;
+        await this.submitListAddress();
+        wx.showModal({
+            title: '温馨提示',
+            content: modalContent,
+            showCancel: false,
+            success: () => wx.navigateBack(),
+        });
 
     },
 
-    // 获取级联选择器数据
+    // 向后端提交地址请求
+    async submitListAddress() {
+        let { form, formBackEnd, areacode, type, id, qqLocation } = this.data;
+        let { location, ad_info } = qqLocation;
+        let { lat: latitude, lng: longitude } = location;
+        let { province, city, district } = ad_info;
+        let index = form.findIndex(item => item.key === 'address');
+        form[index].value = [province, city, district];
+        let finalForm = {};
+        // 遍历对应字段表
+        for (let [k, v] of Object.entries(formBackEnd)) {
+            // 遍历表单数组
+            for (let elem of form.values()) {
+                if (elem.key === k && Array.isArray(v)) {
+                    // 地址
+                    v.forEach((item, index) => { finalForm[item] = elem.value[index] });
+                } else if (elem.key === k && !Array.isArray(v)) {
+                    finalForm[v] = elem.value;
+                }
+            }
+        }
+        let apiMethod = 'addReceiverInfo';
+        // 省市区编号和地址ID
+        finalForm.receiver_areacode = areacode;
+        if (type === 'update') {
+            // 地址编辑操作
+            finalForm.receiver_id = id;
+            apiMethod = 'updateReceiverInfo';
+        }
+        // 国家和是否默认地址
+        finalForm.receiver_country = '';
+        finalForm.receiver_default = 0;
+        // 添加经纬度
+        finalForm.latitude = latitude;
+        finalForm.longtitude = longitude;  // 注意经度字段
+        await api.hei[apiMethod](finalForm);
+    },
+
+    // 校验表单
+    checkAddressForm() {
+        let { form } = this.data;
+        // 表单必填验证
+        let success = form.every(item => item.key === 'houseNumber' || item.key === 'address' || item.value.length);
+        if (!success) {
+            throw new Error('请填写必要的信息');
+        }
+    },
+
+    // 获取地区选择器数据
     async getAreaData() {
         let data = await api.hei.fetchRegionList();
         let { areaList } = data;
         this.setData({ areaList });
-    },
-
-    // 校验表单
-    checkForm() {
-        let { form } = this.data;
-        let success = form.every(item => item.key === 'houseNumber' || item.value.length);
-        if (!success) {
-            throw new Error('请填写完整的地址信息');
-        }
     },
 
     // 弹出地区级联选择器
@@ -511,11 +536,14 @@ Page({
         let { form } = this.data;
         let data = await proxy.chooseLocation();
         let { address, name, latitude, longitude } = data;
+        console.log('微信定位结果', data);
+        let locationObj = { latitude, longitude };
         // 更改详细地址数据
         let index = form.findIndex(item => item.key === 'addressInfo');
         form[index].value = address + name;
         this.setData({
             form,
+            locationObj,
         });
     },
 
@@ -528,8 +556,10 @@ Page({
             throw new Error('获取门店信息失败');
         }
         let distance = getDistance(latitude, longitude, Number(currentStore.latitude), Number(currentStore.longtitude));
+        if (!distance) {
+            throw new Error('计算门店距离失败');
+        }
         distance = Number(distance).toFixed(2);
-        console.log('地址范围', distance);
         let rangeOut = false;
         if (currentStore.distance_limit) {
             rangeOut = Number(distance) >= currentStore.distance_limit;
@@ -537,7 +567,7 @@ Page({
         if (rangeOut) {
             throw new Error('该地址超出门店所配送范围');
         }
-        console.log('门店范围校验通过');
+        console.log(`门店范围校验通过，实际距离${distance}km，限制距离${currentStore.distance_limit}km`);
     },
 });
 
