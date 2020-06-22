45f0afed8ef22fe8a7ed8651c85d7e10322fcf7f
well <well@97866.com>
2020-06-22 19:06:03 +0800

    fix: 扩展nav组件,新增订单列表下拉选择

--- src/components/navbarList/navbarList.js
+++ src/components/navbarList/navbarList.js
@@ -41,16 +41,32 @@ Component({
             value: ''
         },
     },
+    data: {
+        isShowPopup: false,
+        selectedPopupIndex: 0
+    },
     attached() {
         this.setData({ themeColor: app.globalData.themeColor });
     },
     methods: {
-        changeActive(e) {
-            const res = e.currentTarget.dataset;
-            let data = { activeIndex: res.index };
-            data.toView = `tab${res.index}`;
-            this.setData(data);
-            this.triggerEvent('changeNavbarList', res, { bubbles: true });
+        changeActive(ev) {
+            console.log(ev, '--ev');
+            const { navbarListData } = this.data;
+            let index = ev.detail.index || ev.currentTarget.dataset.index;
+            let value = navbarListData[index] && navbarListData[index].value;
+            this.setData({
+                activeIndex: index,
+                toView: `tab${index}`,
+                isShowPopup: false
+            });
+            this.triggerEvent('changeNavbarList', { index, value }, { bubbles: true });
+        },
+        onShowPopup() {
+            let { isShowPopup, activeIndex } = this.data;
+            this.setData({
+                isShowPopup: !isShowPopup,
+                selectedPopupIndex: activeIndex
+            });
         }
     }
 });
