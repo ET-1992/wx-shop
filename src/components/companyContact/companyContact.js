a685807a1a7ea05e2a9ca1d7a6cb3e1c7acf9058
MAOJIE <heyhey@97866.com>
2020-06-29 11:36:32 +0800

    feat: 个人中心联系企业微信客服接入后端数据

--- src/components/companyContact/companyContact.js
+++ src/components/companyContact/companyContact.js
@@ -1,20 +1,16 @@
 Component({
     properties: {
+        // 插件客服ID
         plugid: {
             type: String,
             value: '',
         },
+        // 弹窗开关
         show: {
             type: Boolean,
             value: false,
         }
     },
-    // data: {
-    //     showSuccess: {
-    //         type: Boolean,
-    //         value: false,
-    //     }
-    // },
     methods: {
         onClose() {
             let show = false;
@@ -22,30 +18,40 @@ Component({
                 show,
             });
         },
+
+        // 点击客服
         startmessage() {
             wx.showLoading();
         },
+
+        // 完成客服请求
         completemessage(e) {
             let { errcode, name = '', headurl = '' } = e.detail;
+            let showSuccess = false,
+                showFail = false,
+                hasFrend = false,
+                serviceName = '',
+                serviceHeadurl = '';
             wx.hideLoading();
             if (errcode === 0) {
-                this.setData({
-                    showSuccess: true,
-                    name,
-                    headurl,
-                });
+                showSuccess = true;
+                serviceName = name;
+                serviceHeadurl = headurl;
             } else if (errcode === 3006) {
-                this.setData({
-                    showSuccess: true,
-                    name,
-                    headurl,
-                    hasFrend: true,
-                });
+                showSuccess = true;
+                hasFrend = true;
+                serviceName = name;
+                serviceHeadurl = headurl;
             } else {
-                this.setData({
-                    showFail: true,
-                });
+                showFail = true;
             }
+            this.setData({
+                showSuccess,
+                showFail,
+                hasFrend,
+                serviceName,
+                serviceHeadurl,
+            });
         },
     }
 });
