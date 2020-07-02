15a0fda286a4f9d7e32cd01dc2608adad8e71dc1
MAOJIE <heyhey@97866.com>
2020-07-02 16:23:46 +0800

    fix: 企业微信客服修改错误提示，选择门店的门店项样式修改，发现页/推荐页/模块页支持多门店组件

--- src/components/companyContact/companyContact.js
+++ src/components/companyContact/companyContact.js
@@ -26,28 +26,39 @@ Component({
 
         // 完成客服请求
         completemessage(e) {
+            let failContent = {
+                '-3002': '获取插件配置信息失败',
+                '-3004': '用户信息授权失败',
+                '-3005': '客服消息发送失败',
+                '-3006': '发送的客服人员已经和当前用户是好友关系',
+                '-3008': '当前配置没有客服人员',
+            };
             let { errcode, name = '', headurl = '' } = e.detail;
             let showSuccess = false,
                 showFail = false,
                 hasFrend = false,
+                failTip = '',
                 serviceName = '',
                 serviceHeadurl = '';
             wx.hideLoading();
+            console.log('errcode', errcode);
             if (errcode === 0) {
                 showSuccess = true;
                 serviceName = name;
                 serviceHeadurl = headurl;
-            } else if (errcode === 3006) {
+            } else if (errcode === -3006) {
                 showSuccess = true;
                 hasFrend = true;
                 serviceName = name;
                 serviceHeadurl = headurl;
             } else {
                 showFail = true;
+                failTip = failContent[errcode] || '';
             }
             this.setData({
                 showSuccess,
                 showFail,
+                failTip,
                 hasFrend,
                 serviceName,
                 serviceHeadurl,
