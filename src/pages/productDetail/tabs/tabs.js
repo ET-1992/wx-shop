6b45324c5380a6b4c3d2dee11af0d29bc01dfb35
MAOJIE <heyhey@97866.com>
2020-10-13 11:52:43 +0800

    feat: 商品详情添加页面位置的标签导航栏

---
+++ src/pages/productDetail/tabs/tabs.js
@@ -0,0 +1,33 @@
+const app = getApp();
+
+Component({
+    properties: {
+        scrollTop: {
+            type: Number,
+            optionalTypes: [String],
+            value: 0,
+        }
+    },
+    data: {
+        menuRect: {},  // 导航栏右侧信息
+        themeColor: {},
+    },
+    lifetimes: {
+        attached() {
+            this.getPageData();
+        },
+    },
+    methods: {
+        // 页面初始化
+        getPageData() {
+            let { themeColor } = app.globalData;
+            let menuRect = wx.getMenuButtonBoundingClientRect();
+            this.setData({ menuRect, themeColor });
+        },
+        // 点击标签
+        handleClickTab(e) {
+            let { name } = e.detail;
+        },
+    },
+});
+
