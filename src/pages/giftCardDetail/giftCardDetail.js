550f311285dcb6e19fa32fd304cc639d8b045193
MAOJIE <heyhey@97866.com>
2020-11-06 19:08:24 +0800

    feat: 将新页面放进分包，查看礼品卡采用传入页面的参数方式鉴别赠送者和领取者

--- src/pages/giftCardDetail/giftCardDetail.js
+++ src/pages/giftCardDetail/giftCardDetail.js
@@ -7,25 +7,26 @@ Page({
         gCard: {},
         order: {},
         themeColor: {},
-        fromShare: false,
+        order_no: '',  // 订单号
+        gift_no: '',  // 礼品卡号
     },
 
     onLoad(params) {
         console.log(params);
         let { order_no = '', gift_no = '' } = params,
             { themeColor } = app.globalData;
-        Object.assign(this, {
+        this.setData({
             order_no,
             gift_no,
+            themeColor,
         });
-        this.setData({ themeColor });
         this.getDeatil();
-        this.getEnterPages();
     },
 
     // 获取礼品卡详情
     async getDeatil() {
-        let { order_no, gift_no } = this;
+        let { order_no, gift_no } = this.data;
+        // 赠送者查看使用订单号/领取者查看使用礼品卡号
         let { gift, order } = await api.hei.fetchGiftCardDetail({
             order_no,
             gift_no,
@@ -34,15 +35,5 @@ Page({
             gCard: gift,
             order,
         });
-    },
-
-    // 获取页面栈
-    getEnterPages() {
-        const pages = getCurrentPages();
-        let fromShare = false;
-        if (pages.length === 1) {
-            fromShare = true;
-        }
-        this.setData({ fromShare });
     },
 });
