3e1729a327785837103ae75eff08c7df5c52a938
MAOJIE <heyhey@97866.com>
2020-12-03 19:10:52 +0800

    feat: 在展示商品留言时过滤掉未输入的内容

--- src/components/productReMarkPopup/productReMarkPopup.js
+++ src/components/productReMarkPopup/productReMarkPopup.js
@@ -9,6 +9,17 @@ Component({
             value: false,
         }
     },
+    observers: {
+        'form': function(value) {
+            if (!value) { return }
+            // console.log('value', value);
+            let simpleForm = value.filter(item => item.value);
+            this.setData({ simpleForm });
+        }
+    },
+    data: {
+        simpleForm: [],
+    },
     methods: {
         // 关闭商品留言
         onCloseRemark() {
