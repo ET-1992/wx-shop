c12cec7055a06eb18385a6642407e9350d15d8c6
xpeng <die@97866.com>
2023-03-28 16:26:47 +0800

    feat: small feat

--- src/pages-mvp/orderList/orderList.js
+++ src/pages-mvp/orderList/orderList.js
@@ -1,195 +1,150 @@
-import api from "utils/api";
-import { ORDER_STATUS_TEXT, MAGUA_ORDER_STATUS_TEXT } from "constants/index";
-import { valueToText } from "utils/util";
+import api from 'utils/api';
+import { ORDER_STATUS_TEXT, MAGUA_ORDER_STATUS_TEXT } from 'constants/index';
+import { valueToText } from 'utils/util';
 
 const app = getApp();
 
-const o = {
-  magua: MAGUA_ORDER_STATUS_TEXT,
-};
+// const o = {
+//     'magua': MAGUA_ORDER_STATUS_TEXT
+// };
 
-const D_ORDER_STATUS_TEXT =
-  o[app.globalData.defineTypeGlobal] || ORDER_STATUS_TEXT;
+// const D_ORDER_STATUS_TEXT = o[app.globalData.defineTypeGlobal] || ORDER_STATUS_TEXT;
 
-const dataStatus = D_ORDER_STATUS_TEXT.filter((item) => {
-  const o = [1, 10, 2, 3, 5, 4, 1010, 1011];
-  return o.indexOf(item.value) > -1;
-});
+// const dataStatus = D_ORDER_STATUS_TEXT.filter((item) => {
+//     const o = [1, 10, 2, 3, 5, 4, 1010, 1011];
+//     return o.indexOf(item.value) > -1;
+// });
 
-dataStatus.unshift({ text: "全部", value: null });
+// dataStatus.unshift({ text: '全部', value: null });
 
-console.log(dataStatus);
+// console.log(dataStatus);
+
+
+const dataStatus = [
+  { text: '全部', value: null },
+  { text: '待付款', value: 1000 },
+  // { text: "审核中", value: 1010 }, // 转账支付审核中
+  // { text: "待成团", value: 10 },
+  { text: '待发货', value: 2000 },
+  // { text: "待发货", value: 2002 }, // 已打单
+  { text: '已发货', value: 3000 },
+  // { text: '已部分发货', value: 3001 },
+  // { text: "审核驳回", value: 1011 }, // 转账支付审核驳回
+  { text: '退款中', value: 5000 },
+  { text: '已完成', value: 4000 },
+  // { text: '订单关闭', value: 7000 },
+  // { text: '系统关闭', value: 8000 },
+  { text: '退款成功', value: 6000 },
+  // { text: "待赠送", value: 2001 }, // 礼品卡购买成功
+  // { text: '订单已取消', value: 5 },
+];
 
 Page({
-  data: {
-    orders: [
-      {
-        thumbnail:
-          "http://cdn2.wpweixin.com/wp-content/uploads/sites/371/2022/04/1649322238-%E9%99%B6%E7%84%B6%E8%BD%A9.jpg?imageMogr2/auto-orient/thumbnail/!540x540r/gravity/Center/crop/540x540/quality/70|watermark/1/image/aHR0cDovL2NkbjIud3B3ZWl4aW4uY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy9zaXRlcy8zNzEvMjAyMi8wMS8xNjQyMDU4NDQ3LUdyb3VwLTEyOS5wbmc=/dx/0/dy/0/ws/1#",
-        title:
-          "无线吸拖吸尘器，26000Pa大吸无线吸拖吸尘器，26000Pa大吸力…无线吸拖吸尘器，26000Pa大吸力",
-        sku_property_names: "",
-        statusText: "待付款",
-        status: 1,
-        price: "1321",
-        category:
-          "白色/型号一/白色/型号一/白色/型号一顶故事梗概大姑的风格复古风的观点",
-        categoryNum: 1,
-      },
-      {
-        thumbnail:
-          "http://cdn2.wpweixin.com/wp-content/uploads/sites/371/2022/04/1649322238-%E9%99%B6%E7%84%B6%E8%BD%A9.jpg?imageMogr2/auto-orient/thumbnail/!540x540r/gravity/Center/crop/540x540/quality/70|watermark/1/image/aHR0cDovL2NkbjIud3B3ZWl4aW4uY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy9zaXRlcy8zNzEvMjAyMi8wMS8xNjQyMDU4NDQ3LUdyb3VwLTEyOS5wbmc=/dx/0/dy/0/ws/1#",
-        title:
-          "无线吸拖吸尘器，26000Pa大吸无线吸拖吸尘器，26000Pa大吸力…无线吸拖吸尘器，26000Pa大吸力",
-        sku_property_names: "",
-        statusText: "待发货",
-        status: 2,
-        price: "1321",
-        category:
-          "白色/型号一/白色/型号一/白色/型号一顶故事梗概大姑的风格复古风的观点",
-        categoryNum: 1,
-      },
-      {
-        thumbnail:
-          "http://cdn2.wpweixin.com/wp-content/uploads/sites/371/2022/04/1649322238-%E9%99%B6%E7%84%B6%E8%BD%A9.jpg?imageMogr2/auto-orient/thumbnail/!540x540r/gravity/Center/crop/540x540/quality/70|watermark/1/image/aHR0cDovL2NkbjIud3B3ZWl4aW4uY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy9zaXRlcy8zNzEvMjAyMi8wMS8xNjQyMDU4NDQ3LUdyb3VwLTEyOS5wbmc=/dx/0/dy/0/ws/1#",
-        title:
-          "无线吸拖吸尘器，26000Pa大吸无线吸拖吸尘器，26000Pa大吸力…无线吸拖吸尘器，26000Pa大吸力",
-        sku_property_names: "",
-        statusText: "已发货",
-        status: 3,
-        price: "1321",
-        category:
-          "白色/型号一/白色/型号一/白色/型号一顶故事梗概大姑的风格复古风的观点",
-        categoryNum: 1,
-      },
-      {
-        thumbnail:
-          "http://cdn2.wpweixin.com/wp-content/uploads/sites/371/2022/04/1649322238-%E9%99%B6%E7%84%B6%E8%BD%A9.jpg?imageMogr2/auto-orient/thumbnail/!540x540r/gravity/Center/crop/540x540/quality/70|watermark/1/image/aHR0cDovL2NkbjIud3B3ZWl4aW4uY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy9zaXRlcy8zNzEvMjAyMi8wMS8xNjQyMDU4NDQ3LUdyb3VwLTEyOS5wbmc=/dx/0/dy/0/ws/1#",
-        title:
-          "无线吸拖吸尘器，26000Pa大吸无线吸拖吸尘器，26000Pa大吸力…无线吸拖吸尘器，26000Pa大吸力",
-        sku_property_names: "",
-        statusText: "已完成",
-        status: 4,
-        price: "1321",
-        category:
-          "白色/型号一/白色/型号一/白色/型号一顶故事梗概大姑的风格复古风的观点",
-        categoryNum: 1,
-      },
-      {
-        thumbnail:
-          "http://cdn2.wpweixin.com/wp-content/uploads/sites/371/2022/04/1649322238-%E9%99%B6%E7%84%B6%E8%BD%A9.jpg?imageMogr2/auto-orient/thumbnail/!540x540r/gravity/Center/crop/540x540/quality/70|watermark/1/image/aHR0cDovL2NkbjIud3B3ZWl4aW4uY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy9zaXRlcy8zNzEvMjAyMi8wMS8xNjQyMDU4NDQ3LUdyb3VwLTEyOS5wbmc=/dx/0/dy/0/ws/1#",
-        title:
-          "无线吸拖吸尘器，26000Pa大吸无线吸拖吸尘器，26000Pa大吸力…无线吸拖吸尘器，26000Pa大吸力",
-        sku_property_names: "",
-        statusText: "订单已取消",
-        status: 5,
-        price: "1321",
-        category:
-          "白色/型号一/白色/型号一/白色/型号一顶故事梗概大姑的风格复古风的观点",
-        categoryNum: 1,
-      },
-    ],
-    next_cursor: 0,
-    activeIndex: 0,
-    isRefresh: true,
-    navbarListData: dataStatus,
-    selectedStatus: null,
-  },
+    data: {
+        orders: [],
+        next_cursor: 0,
+        activeIndex: 0,
+        isRefresh: true,
+        navbarListData: dataStatus,
+        selectedStatus: null
+    },
 
-  async loadOrders() {
-    const { next_cursor, isRefresh, orders, selectedStatus } = this.data;
-    const queryOption = { cursor: next_cursor };
-    if (selectedStatus) {
-      queryOption.status = selectedStatus;
+    async loadOrders() {
+        const { next_cursor, isRefresh, orders, selectedStatus } = this.data;
+        const queryOption = { cursor: next_cursor };
+        if (selectedStatus) {
+            queryOption.status = selectedStatus;
+        }
+        if (isRefresh) {
+            wx.showLoading();
+        }
+        const data = await api.hei.fetchOrderList(queryOption);
+        const formatedOrders = data.orders.map((order) => {
+            const statusCode = Number(order.status);
+            order.statusCode = statusCode;
+            order.statusText = valueToText(ORDER_STATUS_TEXT, Number(order.status));
+            order.productCount = order.items.reduce((count, item) => {
+                return count + Number(item.quantity);
+            }, 0);
+            return order;
+        });
+
+        const newOrders = isRefresh ? formatedOrders : orders.concat(formatedOrders);
+        this.setData({
+            orders: newOrders,
+            isRefresh: false,
+            next_cursor: data.next_cursor,
+            config: data.config
+        });
+
+        wx.hideLoading();
+        console.log(this.data);
+    },
+
+    changeNavbarList(ev) {
+        const { index, value } = ev.detail;
+        this.setData({
+            selectedStatus: value,
+            activeIndex: index,
+            isRefresh: true,
+            next_cursor: 0
+        });
+        this.loadOrders();
+    },
+
+    async onLoad({ status }) {
+        const state = Number(status) || null;
+        const { themeColor } = app.globalData;
+        const { navbarListData } = this.data;
+        let index = navbarListData.findIndex((item) => {
+            return item.value === state;
+        });
+
+        this.setData({
+            selectedStatus: state,
+            activeIndex: index,
+            themeColor,
+            globalData: app.globalData,
+        });
+        this.loadOrders();
+    },
+
+    async onPullDownRefresh() {
+        this.setData({ isRefresh: true, next_cursor: 0 });
+        await this.loadOrders();
+        wx.stopPullDownRefresh();
+    },
+
+    async onReachBottom() {
+        const { next_cursor } = this.data;
+        if (!next_cursor) { return }
+        this.loadOrders();
+    },
+
+    onConfirmOrder(ev) {
+        const { orderNo, orderIndex } = ev.detail;
+        const updateData = {};
+        updateData[`orders[${orderIndex}].statusCode`] = 4;
+        updateData[`orders[${orderIndex}].status`] = 4;
+        updateData[`orders[${orderIndex}].statusText`] = valueToText(ORDER_STATUS_TEXT, 4000);
+        this.setData(updateData);
+    },
+
+    onPayOrder(ev) {
+        const { orderNo, orderIndex } = ev.detail;
+        const updateData = {};
+        updateData[`orders[${orderIndex}].statusCode`] = 2;
+        updateData[`orders[${orderIndex}].status`] = 2;
+        updateData[`orders[${orderIndex}].statusText`] = valueToText(ORDER_STATUS_TEXT, 2000);
+        this.setData(updateData);
+    },
+    onCloseOrder(ev) {
+        const { orderNo, orderIndex } = ev.detail;
+        console.log(orderIndex);
+        const updateData = {};
+        updateData[`orders[${orderIndex}].statusCode`] = 7;
+        updateData[`orders[${orderIndex}].status`] = 7;
+        updateData[`orders[${orderIndex}].statusText`] = valueToText(ORDER_STATUS_TEXT, 7000);
+        this.setData(updateData);
     }
-    if (isRefresh) {
-      wx.showLoading();
-    }
-    const data = await api.hei.fetchOrderList(queryOption);
-    const formatedOrders = data.orders.map((order) => {
-      const statusCode = Number(order.status);
-      order.statusCode = statusCode;
-      order.statusText = valueToText(D_ORDER_STATUS_TEXT, Number(order.status));
-      order.productCount = order.items.reduce((count, item) => {
-        return count + Number(item.quantity);
-      }, 0);
-      return order;
-    });
-
-    const newOrders = isRefresh
-      ? formatedOrders
-      : orders.concat(formatedOrders);
-    this.setData({
-      orders: newOrders,
-      isRefresh: false,
-      next_cursor: data.next_cursor,
-      config: data.config,
-    });
-
-    wx.hideLoading();
-    console.log(this.data);
-  },
-
-  changeNavbarList(ev) {
-    const { index, value } = ev.detail;
-    this.setData({
-      selectedStatus: value,
-      activeIndex: index,
-      isRefresh: true,
-      next_cursor: 0,
-    });
-    // this.loadOrders();
-  },
-
-  async onLoad({ status }) {},
-
-  async onPullDownRefresh() {
-    this.setData({ isRefresh: true, next_cursor: 0 });
-    await this.loadOrders();
-    wx.stopPullDownRefresh();
-  },
-
-  async onReachBottom() {
-    const { next_cursor } = this.data;
-    if (!next_cursor) {
-      return;
-    }
-    this.loadOrders();
-  },
-
-  onConfirmOrder(ev) {
-    const { orderNo, orderIndex } = ev.detail;
-    const updateData = {};
-    updateData[`orders[${orderIndex}].statusCode`] = 4;
-    updateData[`orders[${orderIndex}].status`] = 4;
-    updateData[`orders[${orderIndex}].statusText`] = valueToText(
-      D_ORDER_STATUS_TEXT,
-      4
-    );
-    this.setData(updateData);
-  },
-
-  onPayOrder(ev) {
-    const { orderNo, orderIndex } = ev.detail;
-    const updateData = {};
-    updateData[`orders[${orderIndex}].statusCode`] = 2;
-    updateData[`orders[${orderIndex}].status`] = 2;
-    updateData[`orders[${orderIndex}].statusText`] = valueToText(
-      D_ORDER_STATUS_TEXT,
-      2
-    );
-    this.setData(updateData);
-  },
-  onCloseOrder(ev) {
-    const { orderNo, orderIndex } = ev.detail;
-    console.log(orderIndex);
-    const updateData = {};
-    updateData[`orders[${orderIndex}].statusCode`] = 7;
-    updateData[`orders[${orderIndex}].status`] = 7;
-    updateData[`orders[${orderIndex}].statusText`] = valueToText(
-      D_ORDER_STATUS_TEXT,
-      7
-    );
-    this.setData(updateData);
-  },
 });
--- src/pages/miaoshaList/miaoshaList.js
+++ src/pages/miaoshaList/miaoshaList.js
@@ -1,105 +1,93 @@
-import api from "utils/api";
-import { onDefaultShareAppMessage } from "utils/pageShare";
-import { CONFIG } from "constants/index";
+import api from 'utils/api';
+import { onDefaultShareAppMessage } from 'utils/pageShare';
+import { CONFIG } from 'constants/index';
 const app = getApp();
 
 // 创建页面实例对象
 Page({
-  // 页面的初始数据
-  data: {
-    products: [],
-    isRefresh: false,
-    next_cursor: 0,
-    page_title: "",
-    share_title: "",
-    share_image: "",
-    post_type_title: "",
-    taxonomy_title: "",
-    isLoading: true,
-    current_page: 1,
-  },
+    // 页面的初始数据
+    data: {
+        products: [],
+        isRefresh: false,
+        next_cursor: 0,
+        page_title: '',
+        share_title: '',
+        share_image: '',
+        post_type_title: '',
+        taxonomy_title: '',
+        isLoading: true,
+        current_page: 1
+    },
 
-  async loadProducts() {
-    let { categoryId, isRefresh, products, type, current_page } = this.data;
-    let promotion_type = "miaosha_enable";
-    if (type === "bargain") {
-      promotion_type = "bargain_enable";
-    }
-    if (type === "seckill") {
-      promotion_type = "seckill_enable";
-    }
-    if (type === "groupon") {
-      promotion_type = "groupon_enable";
-    }
-    const data = await api.hei.fetchProductList({
-      promotion_type: promotion_type,
-      paged: current_page,
-      product_category_id: categoryId || "",
-    });
-    current_page++;
-    const newProducts = isRefresh
-      ? data.products
-      : products.concat(data.products);
-    wx.setNavigationBarTitle({
-      title: data.page_title,
-    });
-    const {
-      miaosha_banner = "",
-      seckill_banner = "",
-      bargain_banner = "",
-      groupon_banner = "",
-      total_pages,
-      share_image,
-      share_title,
-    } = data;
-    this.setData({
-      products: newProducts,
-      isRefresh: false,
-      miaosha_banner,
-      seckill_banner,
-      bargain_banner,
-      groupon_banner,
-      total_pages,
-      isLoading: false,
-      share_image,
-      share_title,
-      current_page,
-    });
-    return data;
-  },
+    async loadProducts() {
+        let { categoryId, isRefresh, products, type, current_page } = this.data;
+        let promotion_type = 'miaosha_enable';
+        if (type === 'bargain') {
+            promotion_type = 'bargain_enable';
+        }
+        if (type === 'seckill') {
+            promotion_type = 'seckill_enable';
+        }
+        if (type === 'groupon') {
+            promotion_type = 'groupon_enable';
+        }
+        const data = await api.hei.fetchProductList({
+            promotion_type: promotion_type,
+            paged: current_page,
+            product_category_id: categoryId || ''
+        });
+        current_page++;
+        const newProducts = isRefresh ? data.products : products.concat(data.products);
+        wx.setNavigationBarTitle({
+            title: data.page_title
+        });
+        const { miaosha_banner = '', seckill_banner = '', bargain_banner = '', groupon_banner = '', total_pages, share_image, share_title } = data;
+        this.setData({
+            products: newProducts,
+            isRefresh: false,
+            miaosha_banner,
+            seckill_banner,
+            bargain_banner,
+            groupon_banner,
+            total_pages,
+            isLoading: false,
+            share_image,
+            share_title,
+            current_page
+        });
+        return data;
+    },
 
-  async onLoad({ type = "miaosha", categoryId = "" }) {
-    console.log("type", type);
-    const { themeColor } = app.globalData;
-    const config = wx.getStorageSync(CONFIG);
-    const { style_type: tplStyle = "default" } = config;
-    this.setData({
-      themeColor,
-      type,
-      tplStyle,
-      categoryId,
-    });
-    this.loadProducts();
-  },
+    async onLoad({ type = 'miaosha', categoryId = '' }) {
+        console.log('type', type);
+        const { themeColor } = app.globalData;
+        const config = wx.getStorageSync(CONFIG);
+        const { style_type: tplStyle = 'default' } = config;
+        this.setData({
+            themeColor,
+            type,
+            tplStyle,
+            categoryId
+        });
+        this.loadProducts();
+    },
 
-  async onPullDownRefresh() {
-    this.setData({
-      isRefresh: true,
-      isLoading: true,
-      current_page: 1,
-    });
-    await this.loadProducts();
-    wx.stopPullDownRefresh();
-  },
+    async onPullDownRefresh() {
+        this.setData({
+            isRefresh: true,
+            isLoading: true,
+            current_page: 1
+        });
+        await this.loadProducts();
+        wx.stopPullDownRefresh();
+    },
 
-  async onReachBottom() {
-    const { current_page, total_pages } = this.data;
-    if (current_page > total_pages) {
-      return;
-    }
-    this.loadProducts();
-  },
+    async onReachBottom() {
+        const { current_page, total_pages } = this.data;
+        if (current_page > total_pages) { return }
+        this.loadProducts();
+    },
 
-  // 页面分享设置
-  onShareAppMessage: onDefaultShareAppMessage,
+    // 页面分享设置
+    onShareAppMessage: onDefaultShareAppMessage
 });
